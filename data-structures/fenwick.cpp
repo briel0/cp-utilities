e834d319b39f6de020bee2a6eae496a4ee6e436f
Gabriel Abreu <g.abreu@aluno.ufabc.edu.br>
2023-09-12 12:16:11 -0300

    create fenwick.cpp

---
+++ data-structures/fenwick.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+vector<int> bit;
+
+//PRA SOMAR VAI SUBTRAINDO O BIT MENOS SIGNIFICATIVO
+int sum(int pos){
+    int ans = 0;
+    while(pos > 0){
+        ans += bit[pos];
+        pos -= pos & (-pos);
+    }
+    return ans;
+}
+
+//PRA ATUALIZAR VAI SOMANDO O BIT MENOS SIGNIFICATIVO
+void update(int pos, int novo){
+    while(pos <= n){
+        bit[pos] += novo;
+        pos += pos & (-pos);
+    }
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> n;
+    bit.resize(n + 1);
+    for(int i = 1; i <= n; i++){
+        int p;
+        cin >> p;
+        update(i, p);
+    }
+    
+}
