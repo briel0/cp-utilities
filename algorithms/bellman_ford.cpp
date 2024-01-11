211313af36d731d34f6afafad7bd715a2d0f0702
Gabriel Abreu <g.abreu@aluno.ufabc.edu.br>
2024-01-11 02:46:27 -0300

    create bellman_ford.cpp

---
+++ algorithms/bellman_ford.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+ 
+    int n, m;
+    cin >> n >> m;
+    vector<array<int, 3>> edges(m);
+    for(auto &[a, b, peso] : edges){
+        cin >> a >> b >> peso;
+    }
+    vector<int> dist(n + 1, LLONG_MAX);
+    dist[1] = 0;
+    for(int i = 0; i < n - 1; i++){
+        bool mudou = 0;
+        for(auto &[a, b, peso] : edges){
+            if(dist[a] != LLONG_MAX){
+                if(dist[a] + peso < dist[b]){
+                    dist[b] = dist[a] + peso;
+                    mudou = 1;
+                }
+            }
+        }
+        if(!mudou){
+            break;
+        }
+    }
+    for(int i = 1; i <= n; i++){
+        cout << dist[i] << ' ';
+    }
+}
