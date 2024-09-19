85f7391fd305f3fc1a947c653b10fb0fd3366b98
Gabriel Abreu <g.abreu@aluno.ufabc.edu.br>
2024-09-18 23:49:14 -0300

    update union-find.cpp

--- data-structures/union-find.cpp
+++ data-structures/union-find.cpp
@@ -1,26 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> p, s;
-//parenting and size of sets
-
-int find_set(int v){
-    return (p[v] == v ? v : p[v] = find_set(p[v]));
-    //reconstruindo a árvore de forma a "subir mais rápido"
-}
-
-void union_set(int v, int q){
-    v = find_set(v);
-    q = find_set(q);
-    if(v != q){
-        if(s[v] < s[q]){ // o v é o de maior tamanho
-            swap(v, q);
-        }
-        //o parente do de menor tamanho se torna o de maior
-        p[q] = v;
-        s[v] += s[q];
+struct disjointset{
+    vector<int> p, s; 
+    
+    disjointset(int n) : p(n + 1), s(n + 1){
+        iota(p.begin(), p.end(), 0);
     }
-}
+    
+    int find_set(int v){
+        return (p[v] == v ? v : p[v] = find_set(p[v])); 
+    }
+    
+    void union_set(int v, int q){
+        v = find_set(v);
+        q = find_set(q);
+        if(v != q) {
+            if(s[v] < s[q]) { 
+                swap(v, q);
+            }
+            p[q] = v;
+            s[v] += s[q];
+        }
+    }
+};
 
 int main(){
     ios_base::sync_with_stdio(false);
