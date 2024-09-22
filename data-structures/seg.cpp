7b838be8dae16160ab9572907f0c907ecb621687
Gabriel Abreu <g.abreu@aluno.ufabc.edu.br>
2024-09-21 21:13:56 -0300

    update seg.cpp

--- data-structures/seg.cpp
+++ data-structures/seg.cpp
@@ -1,83 +1,73 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define int long long
 
-//defina a operação previamente
-//RMQ nesse caso
 int combina(int a, int b){
-    return min(a, b);
+    return a + b;
 }
 
-vector<int> original, seg;
-int a, b, n, _new, idx; 
-// a -> left, b-> right, _new-> novo valor, idx-> posicao
+struct SegmentTree {
+    vector<int> original, seg;
+    int n;
 
-void build(int node = 1, int l = 1, int r = n){
-    if(l == r){
-        seg[node] = original[l];
+    SegmentTree(int size) : original(size + 1), seg(4 * size + 1){
+        n = size;
     }
-    else{
-        int mid = (l + r) / 2;
-        build(2*node, l, mid);
-        build(2*node + 1, mid + 1, r);
-        seg[node] = combina(seg[2*node], seg[2*node + 1]);
-    }
-}
 
-void update(int node = 1, int l = 1, int r = n){
-    if(l == r){
-        seg[node] = _new;
-        return;
-    }
-    int mid = (l + r) / 2;
-    if(idx <= mid){
-        update(2*node, l, mid);
-    }
-    else{
-        update(2*node + 1, mid + 1, r);
-    }
-    seg[node] = combina(seg[2*node], seg[2*node + 1]);
-}
-
-int query(int node = 1, int l = 1, int r = n){
-    if(l > b || r < a){
-         return INT_MAX;
-    }
-    if(l >= a && r <= b){
-         return seg[node];
-    }
-    int mid = (l + r) / 2;
-    return combina(query(2*node, l, mid), query(2*node + 1, mid + 1, r));
-}
-
-int main(){
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    
-    int q;
-    cin >> n >> q;
-    
-    original.resize(n + 1);
-    seg.resize(4*n + 1);
-    
-    for(int i = 1; i <= n; i++){
-        cin >> original[i];
-    }
-    
-    build();
-    
-    while(q--){
-        char o;
-        cin >> o;
-        if(o == 'U'){ //update
-            cin >> idx >> _new;
-            update();
-        } 
-        else{ //query
-            cin >> a >> b;
-            cout << query() << '\n';
+    void build(int node = 1, int l = 1, int r = 0){
+        if(r == 0){
+            r = n;
+        }
+        if(l == r){
+            seg[node] = original[l];
+        }
+        else{
+            int mid = (l + r) / 2;
+            build(2 * node, l, mid);
+            build(2 * node + 1, mid + 1, r);
+            seg[node] = combina(seg[2 * node], seg[2 * node + 1]);
         }
     }
+
+    void update(int idx, int _new, int node = 1, int l = 1, int r = 0){
+        if(r == 0){
+            r = n;
+        }
+        if(l == r){
+            seg[node] = _new;
+            return;
+        }
+        int mid = (l + r) / 2;
+        if(idx <= mid){
+            update(idx, _new, 2 * node, l, mid);
+        }
+        else{
+            update(idx, _new, 2 * node + 1, mid + 1, r);
+        }
+        seg[node] = combina(seg[2 * node], seg[2 * node + 1]);
+    }
+
+    int query(int a, int b, int node = 1, int l = 1, int r = 0){
+        if(r == 0){
+            r = n;
+        }
+        if(l > b || r < a){
+            return 0;
+        }
+        if(l >= a && r <= b){
+            return seg[node];
+        }
+        int mid = (l + r) / 2;
+        return combina(query(a, b, 2 * node, l, mid), query(a, b, 2 * node + 1, mid + 1, r));
+    }
+};
+
+signed main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+    SegmentTree seg(n);
     
 }
-/*
-*/
