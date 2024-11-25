9d33bf9d6a5854d2b549c5e1d6b53982fbf40712
Gabriel Abreu <g.abreu@aluno.ufabc.edu.br>
2024-11-25 13:45:19 -0300

    update lazy_seg.cpp

--- data-structures/lazy_seg.cpp
+++ data-structures/lazy_seg.cpp
@@ -1,83 +1,105 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define int long long
 
-//segtree with lazy prop for sum queries
+struct SegmentTree{
+    int n;
+    vector<int> seg, lazy, original;
 
-int la, lb, _new, n;
-vector<int> seg, lazy, original;
-
-int op(int a, int b){
-    return a + b;
-}
-
-void build(int node = 1, int l = 1, int r = n){
-    if(l == r){
-        seg[node] = original[l];
+    SegmentTree(int size) : n(size){
+        original.assign(n + 1, 0);
+        seg.assign(4 * n + 1, 0);
+        lazy.assign(4 * n + 1, 0);
     }
-    else{
-        int mid = (l + r) / 2;
-        build(2*node, l, mid);
-        build(2*node + 1, mid + 1, r);
-        seg[node] = op(seg[2*node], seg[2*node + 1]);
-    }
-}
 
-void prop(int node, int l, int r){
-    if(!lazy[node]) return;
-    seg[node] += (r - l + 1) * lazy[node]; //(r - l + 1) o "suficiente" para ser distribuido nos nós restantes, alterar a depender da operação 
-    if(l != r){
-        lazy[2*node] += lazy[node];
-        lazy[2*node + 1] += lazy[node];
+    int op(int a, int b){
+        return a + b;
     }
-    lazy[node] = 0;
-}
 
-int query(int node = 1, int l = 1, int r = n){
-    prop(node, l, r);
-    if(l > lb || r < la) return 0;
-    if(l >= la && r <= lb) return seg[node];
-    int mid = (l + r) / 2;
-    return op(query(2*node, l, mid), query(2*node + 1, mid + 1, r));
-}
-
-void update(int node = 1, int l = 1, int r = n){
-    prop(node, l, r);
-    if(l > lb || r < la) return;
-    if(l >= la && r <= lb){
-        lazy[node] += _new;
-        prop(node, l, r);
-    }
-    else{
-        int mid = (l + r) / 2;
-        update(2*node, l, mid);
-        update(2*node + 1, mid + 1, r);
-        seg[node] = op(seg[2*node], seg[2*node + 1]);
-    }
-}
-
-int main(){
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    
-    int t;
-    cin >> t;
-    while(t--){
-        int c;
-        cin >> n >> c;
-        lazy.assign(4*n + 1, 0);
-        seg.assign(4*n + 1, 0);
-        while(c--){
-            int o;
-            cin >> o;
-            if(!o){
-                cin >> la >> lb >> _new;
-                update();
-            }
-            else{
-                cin >> la >> lb;
-                cout << query() << '\n';
-            }
+    void build(int node = 1, int l = 1, int r = 0){
+        if(r == 0){
+            r = n;
+        }
+        if(l == r){
+            seg[node] = original[l];
+        }
+        else{
+            int mid = (l + r) / 2;
+            build(2 * node, l, mid);
+            build(2 * node + 1, mid + 1, r);
+            seg[node] = op(seg[2 * node], seg[2 * node + 1]);
         }
     }
 
+    void prop(int node, int l, int r){
+        if(!lazy[node]){
+            return;
+        }
+        seg[node] += (r - l + 1) * lazy[node];
+        if(l != r){
+            lazy[2 * node] += lazy[node];
+            lazy[2 * node + 1] += lazy[node];
+        }
+        lazy[node] = 0;
+    }
+
+    int query(int la, int lb, int node = 1, int l = 1, int r = 0){
+        if(r == 0){
+            r = n;
+        }
+        prop(node, l, r);
+        if(l > lb || r < la){
+            return 0;
+        }
+        if(l >= la && r <= lb){
+            return seg[node];
+        }
+        int mid = (l + r) / 2;
+        return op(query(la, lb, 2 * node, l, mid), query(la, lb, 2 * node + 1, mid + 1, r));
+    }
+
+    void update(int la, int lb, int _new, int node = 1, int l = 1, int r = 0){
+        if(r == 0){
+            r = n;
+        }
+        prop(node, l, r);
+        if(l > lb || r < la){
+            return;
+        }
+        if(l >= la && r <= lb){
+            lazy[node] += _new;
+            prop(node, l, r);
+        }
+        else{
+            int mid = (l + r) / 2;
+            update(la, lb, _new, 2 * node, l, mid);
+            update(la, lb, _new, 2 * node + 1, mid + 1, r);
+            seg[node] = op(seg[2 * node], seg[2 * node + 1]);
+        }
+    }
+};
+
+signed main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n, m;
+    cin >> n >> m;
+    SegmentTree seg(n + 1);
+    while(m--){
+        int op;
+        cin >> op;
+        if(op == 1){
+            int l, r, v;
+            cin >> l >> r >> v;
+            l++;
+            seg.update(l, r, v);
+        }
+        else{
+            int l, r;
+            cin >> l >> r;
+            l++;
+            cout << seg.query(l, r) << '\n';
+        }
+    }
 }
